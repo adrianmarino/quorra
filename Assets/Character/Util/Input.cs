c9c0d9fd215c2f24c593e6185ece4580aea7d509
Adrian Norberto Marino <adrian.marino@almundo.com>
2017-04-09 19:15:56 -0300

    add mouse look

---
+++ Assets/Character/Util/Input.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+	public class Input
+	{
+		public static void HideCursor (bool value)
+		{
+			if (value)
+				HideCursor ();
+			else
+				ShowCursor ();
+		}
+
+		public static void HideCursor ()
+		{
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+
+		public static void ShowCursor ()
+		{
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		public static Vector2 NextKeyboardHorVerMovementVariation ()
+		{
+			return new Vector2 (
+				NextHorizontalMovementVariation (), 
+				NextVerticalMovementVariation ()
+			);
+		}
+
+		public static Vector2 NextMouseHorVerMovementVariation ()
+		{
+			return new Vector2 (
+				NextMouseHorizontalMovementVariation (), 
+				NextMouseVercalMovementVariation ()
+			);
+		}
+
+		public static float NextMouseHorizontalMovementVariation ()
+		{
+			return NextMovementVariation ("Mouse X");
+		}
+
+		public static float NextMouseVercalMovementVariation ()
+		{
+			return NextMovementVariation ("Mouse Y");
+		}
+
+		public static float NextHorizontalMovementVariation ()
+		{
+			return NextMovementVariation ("Horizontal");
+		}
+
+		public static float NextVerticalMovementVariation ()
+		{
+			return NextMovementVariation ("Vertical");
+		}
+
+		public static float NextMovementVariation (string axisName)
+		{
+			return UnityEngine.Input.GetAxisRaw (axisName);
+		}
+
+		public static bool GetJumpButtonDown ()
+		{
+			return UnityEngine.Input.GetButtonDown ("Jump");
+		}
+
+		public static bool GetJumpButtonUp ()
+		{
+			return UnityEngine.Input.GetButtonUp ("Jump");
+		}
+
+		public static bool GetFireButton ()
+		{
+			return UnityEngine.Input.GetButton ("Fire1");
+		}
+
+		//-----------------------------------------------------------------------------
+		// Constructors
+		//-----------------------------------------------------------------------------
+
+		private Input ()
+		{
+		}
+	}
+}
